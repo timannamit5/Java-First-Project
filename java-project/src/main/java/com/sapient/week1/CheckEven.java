1c48784bd09c46a78f5c26cf8703b1b28acca81a
Namit Sawhney <namit.sawhney95@gmail.com>
2020-04-10 10:51:32 +0530

    first commit

---
+++ java-project/src/main/java/com/sapient/week1/CheckEven.java
@@ -0,0 +1,12 @@
+package com.sapient.week1;
+
+public class CheckEven {
+	public static Boolean check(int n) {
+		if (n % 2 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+}
---
+++ java-project/src/test/java/com/sapient/week1/CheckEvenTest.java
@@ -0,0 +1,48 @@
+package com.sapient.week1;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class CheckEvenTest 
+    extends TestCase
+{
+    
+    public CheckEvenTest( String testName )
+    {
+        super( testName );
+    }
+
+    
+    public static Test suite()
+    {
+        return new TestSuite( CheckEvenTest.class );
+    }
+
+    
+    public void testApp1()
+    {
+        boolean x = CheckEven.check(5);
+        assertTrue( x == false );
+    }
+
+     public void testApp2()
+    {
+        boolean x = CheckEven.check(10);
+        assertTrue( x == true );
+    }
+
+     public void testApp3()
+    {
+        boolean x = CheckEven.check(9);
+        assertTrue( x == false );
+    }
+
+     public void testApp4()
+    {
+        boolean x = CheckEven.check(11);
+        assertTrue( x == false );
+    }
+
+}
